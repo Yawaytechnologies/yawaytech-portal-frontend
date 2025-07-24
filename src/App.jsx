0166c34ae896445a807782035ed93d8ca228e15a
@Jana-dev <janarthanan.m@yawaytech.com>
2025-07-24 12:28:18 +0530

    Initial set up has been done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import React from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-4xl font-bold text-green-500 bg-gray-100 p-4 rounded shadow">Tailwind CSS is working!</div>
+
+    </>
+  )
+}
+
+export default App

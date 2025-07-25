5803da550c2e0ea9690ec525744bc6817ddfcbfa
@Jana-dev <janarthanan.m@yawaytech.com>
2025-07-25 16:33:41 +0530

    yml file has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import React from 'react'
-
+import React from 'react';
 
 function App() {
 
